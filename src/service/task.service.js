37c83731c07047d86e3c487767338d0550cfbca7
Suyog Patil <suyog0057@gmail.com>
2024-09-16 23:08:06 +0530

    add: logger added to the task.service function

--- src/service/task.service.js
+++ src/service/task.service.js
@@ -1,6 +1,9 @@
+const { logger } = require("../config/logger");
+
 const task =  async function task(user_id) {
     console.log(`${user_id}-task completed at-${Date.now()}`)
     // this should be stored in a log file
+    logger.info(`${user_id}-task completed at-${Date.now()}`);
 }
 
 exports.task = task;
