3f283cf1e757231f7a176ce37bc5246cf7c49644
Suyog Patil <suyog0057@gmail.com>
2024-09-18 18:13:47 +0530

    refactor: rate limiter middleware functionality changed

--- src/middleware/rate-limit.middleware.js
+++ src/middleware/rate-limit.middleware.js
@@ -1,24 +1,41 @@
+const { logger } = require("../config/logger");
 const { getRedisClient } = require("../config/redis-config");
+const { userTaskQueue } = require("../config/bullmq-config");
 
-function rateLimitMiddleware({ rateLimitWindowInSeconds, maxRateLimit }) {
-    return async function (req, res, next) {
-        try {
-            req.user_id = req.body.user_id;
-            const redisClient = getRedisClient();
-            const redisUserId = `rate_limit:${req.user_id}:per${rateLimitWindowInSeconds}s`;
+const rateLimitPerUser = async ({ user_id, rateLimitWindowInSeconds, maxRateLimit }) => {
+    const redisClient = getRedisClient();
+    const redisUserId = `rate_limit:${user_id}:per${rateLimitWindowInSeconds}s`;
 
-            const requests = await redisClient.get(redisUserId);
-            if (requests > maxRateLimit) {
-                res.status(429).json({ "message": "too many requests" });
-                return;
-            }
-            await redisClient.incr(redisUserId);
-            await redisClient.expire(redisUserId, rateLimitWindowInSeconds);
-            next();
-        } catch (err) {
-            console.log(`Error in rateLimit function: ${err}`);
+    const requestCount = await redisClient.incr(redisUserId);
+    if (requestCount === 1) {
+        await redisClient.expire(redisUserId, rateLimitWindowInSeconds);
+    } else if (requestCount > maxRateLimit) {
+        await userTaskQueue.add(`Job#${user_id}#${Date.now()}`, {
+            user_id
+        })
+        return true;
+    }
+    return false;
+};
+
+
+async function rateLimitMiddleware(req, res, next) {
+    try {
+        const isSecondLimit = await rateLimitPerUser({ user_id: req.body.user_id, rateLimitWindowInSeconds: 1, maxRateLimit: 1 });
+        if (isSecondLimit) {
+            res.status(202).json({ "message": `Request will be processed after sometime.` });
+            return;
         }
+        const isMinLimit = await rateLimitPerUser({ user_id: req.body.user_id, rateLimitWindowInSeconds: 60, maxRateLimit: 20 });
+        if (isMinLimit) {
+            res.status(202).json({ "message": `Request will be processed after sometime.` });
+            return;
+        }
+        next();
+    } catch (err) {
+        logger.error(`Error in rateLimitMiddleware function: ${err}\nStackTrace:`);
     }
 }
+
 
 exports.rateLimtiMiddleware = rateLimitMiddleware;
--- src/routes/task.js
+++ src/routes/task.js
@@ -3,14 +3,16 @@ const { rateLimtiMiddleware } = require("../middleware/rate-limit.middleware");
 const { task } = require("../service/task.service");
 const router = Router();
 
-router.use(rateLimtiMiddleware({ rateLimitWindowInSeconds: 1, maxRateLimit: 1 }));
-router.use(rateLimtiMiddleware({ rateLimitWindowInSeconds: 60, maxRateLimit: 20 }));
 
-router.post("/", async (req, res) => {
-    const { user_id } = req.body;
-    if (!user_id) return res.status(400).json({ "error": "user_id is required" });
-    await task(user_id);
-    return res.status(200).send({ "message": "task Complete" });
-});
+router.post("/",
+    rateLimtiMiddleware,
+    async (req, res) => {
+        const { user_id } = req.body;
+
+        if (!user_id) return res.status(400).json({ "error": "user_id is required" });
+
+        await task(user_id);
+        return res.status(200).send({ "message": "task Complete" });
+    });
 
 module.exports = router;
